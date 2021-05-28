822db4c643330abcbb505328a6e6912febedc842
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-05-27 18:03:53 -0600

    changing routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
     <div>
       <Wrapper>
-      <Route  path="/" component={About} />
+      <Route  exact path="/" component={About} />
         <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
