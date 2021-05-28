536fc700fc9cef43278782f4067380b0277a2a6f
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-05-27 18:08:57 -0600

    defined switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 //import NavBar from "./components/NavBar/NavBar";
